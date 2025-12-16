a9a1efc0a4c84c03f0bddf9dcef2dadbe5f85143
WaiZawTun-coder <waizawtun.coder@gmail.com>
2025-12-16 22:48:39 +0630

    update: dbconfig

--- public/index.php
+++ public/index.php
@@ -1,25 +1,2 @@
 <?php
-    $server = $_SERVER['SERVER_NAME'] ?? "localhost";
-    if($server == "localhost"){
-        $host = "localhost";
-        $port = "3306";
-        $username = "root";
-        $password = "W@i1Z@w4Tun2002";
-    }else{
-        require_once __DIR__ . '/../loadEnv.php';
-
-        loadEnv(__DIR__ . '/../.env');
-        $host = getenv("DB_HOST");
-        $port = getenv("DB_PORT");
-        $username = getenv("DB_USER");
-        $password = getenv("DB_PASSWORD");
-    }
-
-    $databaseName = "yukai";
-
-    $conn = new mysqli($host, $username, $password, $databaseName, $port);
-    if($conn->connect_error){
-        die("Connection failed: " . $conn->connect_error);
-    }else{
-        echo "Connection successfully to $server";
-    }
+require "../utilities/dbconfig.php";
