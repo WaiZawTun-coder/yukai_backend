aae0becd0697a052832182efad6e3466197da6ce
WaiZawTun-coder <waizawtun.coder@gmail.com>
2025-12-16 23:07:36 +0630

    test: db config

--- public/index.php
+++ public/index.php
@@ -1,2 +1,13 @@
 <?php
 require "../utilities/dbconfig.php";
+
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+
+if($result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+        echo "User ID: " . $row['user_id'] . " - Name: " . $row["username"] . "<br>";
+    }
+}else{
+    echo "0 results found.";
+}
--- utilities/dbconfig.php
+++ utilities/dbconfig.php
@@ -18,5 +18,5 @@
     if($conn->connect_error){
         die("Connection failed: " . $conn->connect_error);
     }else{
-        echo "Connection successfully to $server";
+        echo "Connection successfully to $server <br>";
     }
