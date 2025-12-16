29c31398407e75d8aa1a84badc8a94492c0344cd
WaiZawTun-coder <waizawtun.coder@gmail.com>
2025-12-16 22:04:55 +0630

    update: database connection

--- public/index.php
+++ public/index.php
@@ -1,5 +1,22 @@
 <?php
-    echo json_encode([
-        "status" => "success",
-        "message" => "PHP running on render.com"
-    ]);
+    $server = $_SERVER['SERVER_NAME'] ?? "localhost";
+    if($server == "localhost"){
+        $host = "localhost";
+        $port = "3306";
+        $username = "root";
+        $password = "W@i1Z@w4Tun2002";
+    }else{
+        $host = "database-yukai.j.aivencloud.com";
+        $port = "17662";
+        $username = "avnadmin";
+        $password = "AVNS_pVnMw8HCSmWQ-wNYKNe";
+    }
+
+    $databaseName = "yukai";
+
+    $conn = new mysqli($host, $username, $password, $databaseName, $port);
+    if($conn->connect_error){
+        die("Connection failed: " . $conn->connect_error);
+    }else{
+        echo "Connection successfully to $server";
+    }
