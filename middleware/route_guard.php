e16674b3bdd9f04e466db3c286a89e31bdea53ec
WaiZawTun-coder <waizawtun.coder@gmail.com>
2025-12-17 21:40:36 +0630

    remove: print

--- middleware/route_guard.php
+++ middleware/route_guard.php
@@ -4,10 +4,6 @@ require_once __DIR__ . '/../bootstrap.php';
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $routes = require __DIR__ . '/../routes/access.php';
 
-print_r($routes);
-
-echo $_SERVER['REQUEST_URI'];
-
 function deny(int $code = 401)
 {
     http_response_code($code);
