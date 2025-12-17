0c41ec541233816aa4d5658abf119d25501717c2
WaiZawTun-coder <waizawtun.coder@gmail.com>
2025-12-17 19:04:22 +0630

    update: access.php

--- routes/access.php
+++ routes/access.php
@@ -1,12 +1,24 @@
 <?php
-
-return [
-    'public' => [
-        '/yukai_backend/public/login.php',
-        '/yukai_backend/public/register.php',
-    ],
-
-    'admin' => [
-        '/yukai_backend/public/admin.php',
-    ],
-];
+    $server = $_SERVER['SERVER_NAME'] ?? "localhost";
+    if($server == "localhost"){
+        return [
+            'public' => [
+                '/yukai_backend/public/login.php',
+                '/yukai_backend/public/register.php',
+            ],
+        
+            'admin' => [
+                '/yukai_backend/public/admin.php',
+            ],
+        ];
+    }else{
+        return [
+            'public' => [
+                "/public/login.php",
+                "/public/register.php"
+            ],
+            'admin' => [
+                "/public/admin.php"
+            ]
+        ];
+    }
