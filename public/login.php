0e0514a1a284aaca1895c155d0f6e470f8e144fb
WaiZawTun-coder <waizawtun.coder@gmail.com>
2025-12-17 18:59:39 +0630

    add: comment in login.php

--- public/login.php
+++ public/login.php
@@ -1,14 +1,20 @@
 <?php
+    // check if route is accessible
     require_once __DIR__ . '/../middleware/route_guard.php';
     include "../utilities/dbconfig.php";
 
+    // get input data
     $input = json_decode(file_get_contents('php://input'), true);
     $username = $input["username"];
     $password = $input["password"];
+
+    // response array
+    // {status: boolean, message: string}
     $response = [];
-    
+
+    //check username and email exit
     $checkUsername="select * from users where email=? or username=? ";
-    $checkUserstmt = $conn->prepare($checkUsername);//check username and email
+    $checkUserstmt = $conn->prepare($checkUsername); 
     $checkUserstmt->bind_param("ss", $username, $username);
     $checkUserstmt->execute();
     $result = $checkUserstmt->get_result();  
@@ -18,32 +24,29 @@
         echo json_encode($response);
         exit(); 
     }
-    else{
-        $user = $result->fetch_assoc();
-        $checkCorrect = "select * from users where (username=? or email=?) and password=? ";
-        $checkCorrectstmt = $conn->prepare($checkCorrect);
-        $checkCorrectstmt->bind_param("sss", $username, $username, $password);
-        $checkCorrectstmt->execute();
-        $result= $checkCorrectstmt->get_result();
-        if($result->num_rows==0){
-            $response["status"] = false;
-            $response["message"] = "You should check your password!";
-            echo json_encode($response);
-            exit();
-
-        }
-        else{
-            $response["status"] = true;
-            $response["message"] = "User and Password are correct";
-            echo json_encode($response);
-            exit();
-
-        }
-
-
-    }
-
     
-    
+    // check password correct
+    $user = $result->fetch_assoc();
+    $checkCorrect = "select * from users where (username=? or email=?) and password=? ";
+    $checkCorrectstmt = $conn->prepare($checkCorrect);
+    $checkCorrectstmt->bind_param("sss", $username, $username, $password);
+    $checkCorrectstmt->execute();
+    $result= $checkCorrectstmt->get_result();
 
+    // incorrect password
+    if($result->num_rows==0){
+        $response["status"] = false;
+        $response["message"] = "You should check your password!";
+        echo json_encode($response);
+        exit();
+    }    
+
+    // correct user and password
+    // access granted
+    $_SESSION['user_id'] = $user["user_id"];
+    $response["status"] = true;
+    $response["message"] = "User and Password are correct";
+    echo json_encode($response);
+    exit();
+    
 ?>                                               
