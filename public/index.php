652cf2ee29c9b43328576f50478f7e905e4cb2ef
WaiZawTun-coder <waizawtun.coder@gmail.com>
2025-12-22 18:27:59 +0630

    update: cors policy

--- public/index.php
+++ public/index.php
@@ -1,4 +1,23 @@
 <?php
+
+$allowedOrigins = [
+    "http://localhost:3000",
+    "https://yukai-frontend.vercel.app"
+];
+
+if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
+    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+    header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+}
+
+// Handle preflight OPTIONS request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
 require_once __DIR__ . "/../bootstrap.php";
 require_once __DIR__ . "/../routes/api.php";
 require_once __DIR__ . '/../app/Core/Database.php';
