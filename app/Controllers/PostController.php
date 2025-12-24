7e3758fa20ddd6907635ed48cc2e2aba7d48ebb9
WaiZawTun-coder <waizawtun.coder@gmail.com>
2025-12-24 09:27:10 +0630

    update: post controller

--- app/Controllers/PostController.php
+++ app/Controllers/PostController.php
@@ -3,17 +3,19 @@ namespace App\Controllers;
 use App\Core\Database;
 use App\Core\Response;
 
-class PostController{
+class PostController
+{
 
-    public static function postOutput() {
+    public static function postOutput()
+    {
         $conn = Database::connect();
         // header("Content-Type: application/json");
-        $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
-        $resultsPerPage = 5; 
+        $page = isset($_GET['page']) ? (int) $_GET['page'] : 1;
+        $resultsPerPage = 5;
         $startFrom = ($page - 1) * $resultsPerPage;
 
         //finding post by flitering comment and react and time stamp
-                $sql =("
+        $sql = ("
                     SELECT p.post_id,p.creater_id,p.shared_post_id,p.privacy,p.content,p.is_archived,p.is_draft,p.is_deleted,p.is_shared,p.created_at,p.updated_at,
                     count(distinct r.react_id) AS react_count,
                     count(distinct c.comment_id) AS comment_count,
@@ -22,32 +24,31 @@ class PostController{
                     WHERE p.is_deleted = 0 
                     GROUP BY p.post_id ORDER BY total_engagement DESC,p.created_at DESC 
                     LIMIT $resultsPerPage OFFSET $startFrom");
-                $result = $conn->query($sql);
-                $sqlTotal = "SELECT COUNT(*) AS total_posts FROM posts WHERE is_deleted = 0";
-                $resultTotal = $conn->query($sqlTotal);
-                $rowTotal = $resultTotal->fetch_assoc();
-                $totalPosts = (int)$rowTotal['total_posts'];
-                $totalPages = ceil($totalPosts / $resultsPerPage);
-                
-                // if ($result->num_rows==0) {
-                //     Response::json([
-                //         "status"=>false,
-                //         "message"=>"Post is not found"
-                //     ],404);
-                // }
-                
-                $posts = [];
-                while ($row = $result->fetch_assoc()) {
-                    $posts[] = $row;
-                }
+        $result = $conn->query($sql);
+        $sqlTotal = "SELECT COUNT(*) AS total_posts FROM posts WHERE is_deleted = 0";
+        $resultTotal = $conn->query($sqlTotal);
+        $rowTotal = $resultTotal->fetch_assoc();
+        $totalPosts = (int) $rowTotal['total_posts'];
+        $totalPages = ceil($totalPosts / $resultsPerPage);
 
-            Response::json([
-                "status"=>true,
-                "message"=>"Posts are as follow",
-                "page" => $page,
-                "totalPages"=>$totalPages,
-                "data"=>$posts
-            ]);
+        // if ($result->num_rows==0) {
+        //     Response::json([
+        //         "status"=>false,
+        //         "message"=>"Post is not found"
+        //     ],404);
+        // }
+
+        $posts = [];
+        while ($row = $result->fetch_assoc()) {
+            $posts[] = $row;
+        }
+
+        Response::json([
+            "status" => true,
+            "page" => $page,
+            "totalPages" => $totalPages,
+            "data" => $posts
+        ]);
 
     }
 }
