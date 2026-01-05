5baedb81b252c33bdcf91540d8ea85e2d0e08d38
May Thingyan <maythingyan5098@gmail.com>
2026-01-05 20:58:40 +0630

    api true change

--- routes/api.php
+++ routes/api.php
@@ -7,74 +7,74 @@ use App\Controllers\SaveController;
 // public routes
 Router::add("GET", "/", function () {
     echo json_encode(["status" => true, "message" => "success"]);
-}, false);
+}, true);
 Router::add("POST", "/auth/login", function () {
     AuthController::login();
-}, false);
+}, true);
 Router::add("POST", "/auth/register", function () {
     AuthController::register();
-}, false);
+}, true);
 Router::add("POST", "/auth/register/{username}", function ($username) {
     AuthController::register($username);
-}, false);
+}, true);
 
 Router::add("POST", "/auth/refresh", function () {
     AuthController::refresh();
-}, false);
+}, true);
 
 
 // users
 Router::add("GET", "/api/get-user", function () {
     UserController::getUser();
-},false);
+},true);
 
 Router::add("GET", "/api/profile", function () {
     AuthController::index();
-}, false);
+}, true);
 
 
 // posts
 Router::add("GET", "/api/get-post", function () {
     PostController::getPosts();
-}, false); // get all posts
+}, true); // get all posts
 
 Router::add("GET", "/api/get-user-post", function () {
     PostController::getPostsByUserId();
-}, false); // get posts by user
+}, true); // get posts by user
 
 Router::add("GET", "/api/get-following-post", function () {
     PostController::getFollowingPosts();
-}, false); // get following posts
+}, true); // get following posts
 
 Router::add("GET", "/api/get-post/{post_id}", function ($post_id) {
     PostController::getPostsByPostId($post_id);
-}, false);
+}, true);
 
 Router::add("POST", "/api/create-post", function () {
     PostController::createPost();
-}, false); // create post
+}, true); // create post
 
 Router::add("POST", "/api/react-post", function () {
     PostController::reactPost();
-}, false); // insert react
+}, true); // insert react
 
 Router::add("POST", "/api/comment-post", function () {
     PostController::commentPost();
-}, false); // insert comment
+}, true); // insert comment
 
 Router::add("POST", "/api/delete-comment", function () {
     PostController::commentDelete();
-}, false); // delete comment
+}, true); // delete comment
 
 Router::add("GET", "/api/get-comment", function () {
     PostController::getComments();
-}, false); // get Comments
+}, true); // get Comments
 
 Router::add("POST", "/api/delete-post", function () {
     PostController::postDelete();
-}, false); // delete post
+}, true); // delete post
 
 Router::add("GET", "/api/save-post", function () {
 SaveController::savePost();
-}, false); // save post
+}, true); // save post
 
