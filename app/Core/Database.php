d671383ddb07b02469a7c06487d5ec1e2acf00ba
May Thingyan <maythingyan5098@gmail.com>
2026-02-14 19:12:05 +0630

    Database

--- app/Core/Database.php
+++ app/Core/Database.php
@@ -45,7 +45,7 @@ class Database
             "host" => getenv("DB_HOST") ?: "127.0.0.1",
             "port" => (int) (getenv("DB_PORT") ?: 3306),
             "user" => getenv("DB_USER") ?: "root",
-            "password" => getenv("DB_PASSWORD") ?: "W@i1Z@w4Tun2002",
+            "password" => getenv("DB_PASSWORD") ?: "May1234!@#",
             "database" => getenv("DB_NAME") ?: "yukai"
         ];
     }
